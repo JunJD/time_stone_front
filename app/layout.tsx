42fcc9c9e6752282fc1268291eb51e2c2784c91f
dingjunjie <dingjunjie@ypshengxian.com>
2023-07-20 22:47:13 +0800

    chore: 更新metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,9 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata = {
   title: 'timeStone',
   description: '一个专注于利用碎片时间提升个人能力的网站',
+  manifest: '/manifest.json',
+  viewport: 'width=device-width, initial-scale=1.0',
+  icons: '/testicon.png',
 };
 
 export default function RootLayout({
